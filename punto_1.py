f8425cf5387d8dfc92eafeee814418664fcf3e91
SebasED <johns.estrada@udea.edu.co>
2022-06-01 16:32:47 -0500

    Added punto_1.py and exercise solved

---
+++ punto_1.py
@@ -0,0 +1,38 @@
+""" 
+R2D2 o Arturito es uno de los robots más famosos de la galaxia, debes escribir un 
+programa  que  lo  dibuje  en  segunda  dimensión.
+
+No es obligatorio, pero si el tiempo no es problema para ti puedes también hacer al 
+compañero de R2D2, C-3PO.
+"""
+
+# Show R2D2 in console
+print("\
+      \t      _____      \n\
+      \t     /  () \\    \n\
+      \t   _|_______|_   \n\
+      \t  | | ====  | |  \n\
+      \t  |_|   o   |_|  \n\
+      \t   ||   o   ||   \n\
+      \t   ||___ ___||   \n\
+      \t  |~ \\_____/~ | \n\
+      \t  /=\\  /=\\  /=\\  \n\
+      \t  [ ]  [ ]  [ ]  \n\
+            I'm R2D2   \n")
+
+# Show C-3PO in console
+print("\
+      \t     /~\\         \n\
+      \t    |oo ) .       \n\
+      \t    _\\=/_        \n\
+      \t   /  _  \\       \n\
+      \t  //|/.\\|\\\\    \n\
+      \t  \\\\ \\_/  ||   \n\
+      \t   \\|\\ /| ||    \n\
+      \t   # _ _/  #      \n\
+      \t    | | |         \n\
+      \t    | | |         \n\
+      \t    []|[]         \n\
+      \t    | | |         \n\
+      \t   / ] [ \\       \n\
+          I'm C-3PO")
