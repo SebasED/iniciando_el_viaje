9c7e13c4be685407302f203230bc1e9d667375af
SebasED <johns.estrada@udea.edu.co>
2022-06-01 16:47:06 -0500

    Added punto_4.py and exercise solved

---
+++ punto_4.py
@@ -0,0 +1,23 @@
+""" 
+El siguiente código pretende realizar una media entre 3 números, pero no funciona 
+correctamente. Corrige el error:
+
+    number_1 = 10 
+    number_2 = 20 
+    number_3 = 6.5 
+ 
+    media = number_1 + number_2 + number_3 / 3 
+ 
+    print(f"{media} es la nota media.")
+"""
+
+# Variables
+number_1 = 10
+number_2 = 20
+number_3 = 6.5
+
+# Calculate average
+media = (number_1 + number_2 + number_3) / 3
+
+# Print median
+print(f"{media} is the average grade between {number_1}, {number_2} and {number_3}")
